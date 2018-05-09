9c3f5f48599a15f0aaa67b48594a8b6e121f3240
mikebpech <mike.pechousek@gmail.com>
2018-05-09 14:08:31 -0400

    More clean-up

--- server/main.js
+++ server/main.js
@@ -1,47 +1,6 @@
 import { Meteor } from 'meteor/meteor';
-import SimpleSchema from 'simpl-schema';
-import { Accounts } from 'meteor/accounts-base';
+import '../imports/api/users';
 
 Meteor.startup(() => {
   
-  Accounts.validateNewUser((user) => {
-    const email = user.emails[0].address;
-
-    //Validate if email is valid email.
-    try {
-      new SimpleSchema({
-        email: {
-          type: String,
-          regEx: SimpleSchema.RegEx.Email
-        }
-      }).validate({ email });
-    } catch (e) {
-      throw new Meteor.Error(400, e.message);
-    }
-
-    return true;
-  });
-
-  // const employeeSchema = new SimpleSchema({
-  //   name: {
-  //     type: String,
-  //     min: 1,
-  //     max: 200
-  //   },
-  //   hourlyWage : {
-  //     type: Number,
-  //     min: 1
-  //   },
-  //   email : {
-  //     type: String,
-  //     regEx: SimpleSchema.RegEx.Email
-  //   }
-  // });
-
-  // employeeSchema.validate({
-  //   name: 'michael pechousek',
-  //   hourlyWage: 17,
-  //   email: 'mike@lolzz.ca'
-  // });
-
 });
